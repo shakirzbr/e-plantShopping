75be25f2847e177ceac47515d617a4382e46b370
Shakir <shakirzbr.works@gmail.com>
2025-04-05 20:53:08 -0400

    Task2 - State management using Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -16,8 +16,14 @@ export const CartSlice = createSlice({
         }
       },
     removeItem: (state, action) => {
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+        } else {
         state.items = state.items.filter(item => item.name !== action.payload);
-    },
+        }
+      },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
